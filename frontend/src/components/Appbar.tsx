8658d0976a8c55061b88e4866bdfa2651f7935f9
Raj Thombare <therajthombare@gmail.com>
2024-08-02 17:00:57 +0530

    update logo

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Avatar } from "./BlogCard";
-import { HiOutlinePencilSquare } from "react-icons/hi2";
 import { BiUser } from "react-icons/bi";
 import { GoBookmark } from "react-icons/go";
 import { IoLogOutOutline } from "react-icons/io5";
@@ -19,9 +18,10 @@ const Appbar = () => {
   return (
     <nav className='bg-white text-black border-gray-200 border-b'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-3 px-5'>
-        <a href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
+        <a href='/' className='flex items-center rtl:space-x-reverse'>
+          <img src='/logo.png' alt='article arc logo' width={30} height={30} />
           <span className='self-center text-2xl font-semibold whitespace-nowrap'>
-            Medium
+            rticalArc
           </span>
         </a>
         <div className='flex justify-between basis-20'>
@@ -30,12 +30,11 @@ const Appbar = () => {
               navigate("/publish");
             }}
             type='button'
-            className='flex flex-column items-center text-gray-700 hover:text-gray-900'
-          >
-            <HiOutlinePencilSquare fontSize={24} />
-            <span className='text-sm ml-1'>Write</span>
+            className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-md px-5 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+            Create
           </button>
-          <div className='flex relative items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ml-8'>
+
+          <div className='flex relative items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ml-3'>
             <button
               onClick={toggleMenu}
               type='button'
@@ -43,8 +42,7 @@ const Appbar = () => {
               id='user-menu-button'
               aria-expanded='false'
               data-dropdown-toggle='user-dropdown'
-              data-dropdown-placement='bottom'
-            >
+              data-dropdown-placement='bottom'>
               <span className='sr-only'>Open user menu</span>
               <Avatar
                 name={"Raj"}
@@ -57,8 +55,7 @@ const Appbar = () => {
               className={`z-50 absolute top-8 right-0 ${
                 isOpen ? "block" : "hidden"
               } my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow`}
-              id='user-dropdown'
-            >
+              id='user-dropdown'>
               <div className='px-4 py-3'>
                 <span className='block text-md mt-1 font-semibold text-gray-900'>
                   Raj Thombare
@@ -71,8 +68,7 @@ const Appbar = () => {
                 <li>
                   <a
                     href='#'
-                    className='flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
-                  >
+                    className='flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'>
                     <BiUser fontSize={20} />
                     <span className='ml-1'>Profile</span>
                   </a>
@@ -80,8 +76,7 @@ const Appbar = () => {
                 <li>
                   <a
                     href='#'
-                    className='flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
-                  >
+                    className='flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'>
                     <GoBookmark fontSize={20} />
                     <span className='ml-1'>Bookmark</span>
                   </a>
@@ -89,8 +84,7 @@ const Appbar = () => {
                 <li>
                   <a
                     href='#'
-                    className='flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
-                  >
+                    className='flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'>
                     <IoLogOutOutline fontSize={20} />
                     <span className='ml-1'>Logout</span>
                   </a>
